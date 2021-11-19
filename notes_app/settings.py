1153124c0f4c13219fd2aec94bece4cd63b8c8c7
paurbx <gleroapachii@gmail.com>
2021-11-19 23:37:10 +0300

    heroku deploy changes v2

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'deo@z+$ueyz%!_!_&8ggt%j2vcm*m!@q!$n5adsg$&3+7@xm20'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cryptic-badlands-65500.herokuapp.com']
 
 
 # Application definition
