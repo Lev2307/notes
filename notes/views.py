595b1208c0687514dd83c9ca420c92ccfd6c1d1c
Lev2307 <Abrosimow1@mail.ru>
2021-11-22 17:50:23 +0300

    upd 22Nov

--- notes/views.py
+++ notes/views.py
@@ -40,13 +40,25 @@ class CreateNoteView(LoginRequiredMixin, CreateView):
             return render(request, 'notes/create.html', {"form": form,'obj': obj})
 
 class ReadNotesView(LoginRequiredMixin, ListView):
-    model = Note
     template_name = 'notes/read.html'
-    context_object_name = 'notes'
     paginate_by = 2
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
-        return context
+    def get(self, request, *args, **kwargs):
+        qs = Note.objects.all()
+        page = int(request.GET.get('page', 1))
+        start_index = (page * self.paginate_by) - self.paginate_by
+        end_index = page * self.paginate_by
+        pages_count = math.ceil(len(qs) / self.paginate_by)
+
+        if page > pages_count:
+            return redirect(f'/notes/read/?page={pages_count}')
+        elif page < 1:
+            return redirect('/notes/read/?page=1')
+
+        return render(request, 'notes/read.html', { 'notes_list': qs[start_index:end_index],
+                                                    'current_page': page,
+                                                    'next': page + 1,
+                                                    'prev': page - 1,
+                                                    'pages_count': pages_count,
+                                                    'pages_count_list': range(1, pages_count+1)})
     
