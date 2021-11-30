0798bc4df6f4a1e3daf25be0577acf04c432d77a
paurbx <57190943+paurbx@users.noreply.github.com>
2021-11-30 09:45:38 +0300

    Update models.py

--- notes/models.py
+++ notes/models.py
@@ -16,19 +16,12 @@ class Collection(models.Model):
 
 class Note(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-<<<<<<< HEAD
     collection = models.ForeignKey(Collection, null=True, on_delete=models.SET_NULL)
-    header = models.CharField(max_length = 30)
-    date = models.DateField(auto_now=True)
-    text = models.CharField(max_length = 150)
-    is_important = models.BooleanField(null=True, default=False)
-=======
-    collection = models.ForeignKey(Collections, null=True, on_delete=models.SET_NULL)
     header = models.CharField(max_length = 40)
     date = models.DateField(auto_now=True)
-    text = models.TextField(max_length = 170)
-    is_important = models.BooleanField(null=True)
->>>>>>> e3100fe602e09f1c09d67c326c8f436966f2f403
+    text = models.CharField(max_length = 170)
+    is_important = models.BooleanField(null=True, default=False)
+
 
     class Meta:
         ordering = ['-date']
